e3236ee71b7dc16f2dd4b7f505bbb50130dd66e0
Giada Ferrari <giadaferrari@giadas-mbp.hhk.dk>
2018-05-23 15:13:15 +0200

    MOBILE:navigation complete / language setting functioning

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
 /*this javascript controls what s universal between pages like the burger menu , switching between languages etc.*/
 
-let itaText = document.querySelectorAll('.ita')
-let engText = document.querySelectorAll('.eng')
-
 
 
 /*language settings*/
 
-$('.enSet').on('click', setLanguage)
-
-function setLanguage(){
-
-    itaText.forEach((ita)=> {ita.classList.toggle('hide')})
-    engText.forEach((eng)=> {eng.classList.toggle('hide')});
-
-
-
-}
+let itaText = document.querySelectorAll('.ita')
+let engText = document.querySelectorAll('.eng')
+let itaSample = document.querySelector('.ita')
+let engSample = document.querySelector('.eng')
 
 $('.itSet').on('click', setLanguage)
+$('.enSet').on('click', setLanguage)
 
+function setLanguage(e) {
+   /*with this if statement I check if what I clicked was the italian button and the italian version is NOT already displayed, or if the english button was pressed and the english version is NOT already displayed, then execute my code, which toggles the class hide*/
 
+    if (itaSample.classList.contains('hide')&&e.target.classList.contains('itSet') || engSample.classList.contains('hide')&&e.target.classList.contains('enSet')) {
+        console.log('from e to i ')
+        itaText.forEach((ita) => {
+            ita.classList.toggle('hide')
+        })
+        engText.forEach((eng) => {
+            eng.classList.toggle('hide')
+        });
+
+    }
+
+}
 
 
 
 /*burger menu*/
 
-$('.burger').on ('click',()=>{
-    $('nav').toggleClass('openNav')
-    $('nav').toggleClass('closeNav')
+$('.burger').on('click', () => {
+    $('.navMenu').toggleClass('openNav')
+    $('.navMenu').toggleClass('closeNav')
 })
-
